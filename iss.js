a599ec84939086d93f31463b15c569d0ab669d07
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-14 22:55:41 +0000

    Completed first exercise

--- iss.js
+++ iss.js
@@ -1,7 +1,25 @@
 const request = require('./node_modules/request');
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
 
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
-
-module.exports = { fetchMyIP }
+};
+//const { fetchMyIP } = require('./iss');
+//
+//fetchMyIP((error, ip) => {
+//  if (error) {
+//    console.log("It didn't work!" , error);
+//    return;
+//  }
+//
+//  console.log('It worked! Returned IP:' , ip);
+//});
+module.exports = { fetchMyIP };
